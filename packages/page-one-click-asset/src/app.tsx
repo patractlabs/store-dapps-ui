97b83e37dcec700cac3a82f7e1e744745f63b794
ii-ii-ii <69485494+ii-ii-ii@users.noreply.github.com>
2021-01-24 00:13:30 +1300

    fix registry

--- packages/page-one-click-asset/src/app.tsx
+++ packages/page-one-click-asset/src/app.tsx
@@ -2,22 +2,29 @@ import { useApi, useModal } from '@patract/react-hooks';
 import { Button, PageHeader, PageLayout, PageMain } from '@patract/ui-components';
 import React from 'react';
 import { CreateAssetModal } from './create-asset-modal';
+import { Api } from '@patract/react-components';
+import { ApiContext } from '@patract/react-components/api/api-context';
+import Queue from '@patract/react-components/status/queue';
 
 export const App = () => {
   const { isOpen, onOpen, onClose } = useModal();
+  console.log(ApiContext)
+  const { isApiReady, api } = useApi();
 
-  const { isApiReady } = useApi();
-
-  console.log(isApiReady);
+  // console.log(isApiReady, api);
 
   return (
-    <PageLayout>
-      <PageHeader title='Patra Asset' />
-      <PageMain>
-        <Button onClick={onOpen}>Create Asset</Button>
-        <CreateAssetModal isOpen={isOpen} onClose={onClose} />
-      </PageMain>
-    </PageLayout>
+    <Queue>
+      <Api url='ws://192.168.50.10:9944'>
+        <PageLayout>
+          <PageHeader title='Patra Asset' />
+          <PageMain>
+            <Button onClick={onOpen}>Create Asset</Button>
+            <CreateAssetModal isOpen={isOpen} onClose={onClose} />
+          </PageMain>
+        </PageLayout>
+      </Api>
+    </Queue>
   );
 };
 
--- packages/page-one-click-asset/src/index.tsx
+++ packages/page-one-click-asset/src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 import { Api } from '@patract/react-components';
+import Queue from '@patract/react-components/status/queue';
 import { UIProvider } from '@patract/ui-components';
+
 import ReactDOM from 'react-dom';
 import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
     <UIProvider>
-      <Api url='ws://192.168.50.10:9944'>
-        <App />
-      </Api>
+      <App />
     </UIProvider>
   </React.StrictMode>,
   document.getElementById('root')
