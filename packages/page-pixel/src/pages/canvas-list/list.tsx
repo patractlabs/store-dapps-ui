d30cad11047065226d5c74a9515b711877fb7e33
iiiiiiiii <69485494+ii-ii-ii@users.noreply.github.com>
2021-01-29 17:58:28 +1300

    update

--- packages/page-pixel/src/pages/canvas-list/list.tsx
+++ packages/page-pixel/src/pages/canvas-list/list.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Icon } from '@chakra-ui/react';
+import { Box, Center, Icon, Stack, SimpleGrid } from '@chakra-ui/react';
 import { useApi } from '@patract/react-hooks';
 import React from 'react';
 import { FaPlusCircle } from 'react-icons/fa';
@@ -26,48 +26,49 @@ export const CanvasList: React.FC = () => {
   return (
     <Center sx={{ mt: '18px' }}>
       <Box sx={{ w: '992px' }}>
-        {canvasListArray.map(({ value, id }) => (
+        <SimpleGrid columns={3} spacing={4}>
+          {canvasListArray.map(({ value, id }) => (
+            <Box
+              key={id}
+              onClick={editPaint.bind(null, id)}
+              sx={{
+                display: 'inline-block',
+                w: '320px',
+                h: '180px',
+                verticalAlign: 'top',
+                cursor: 'pointer',
+                borderRadius: '8px',
+                border: '1px solid',
+                borderColor: 'gray.300',
+                _hover: {
+                  borderColor: 'gray.500'
+                }
+              }}
+            >
+              <MiniCanvas canvasObj={hex2Canvas(api.registry, value)} />
+            </Box>
+          ))}
+
           <Box
-            key={id}
-            onClick={editPaint.bind(null, id)}
             sx={{
               display: 'inline-block',
               w: '320px',
               h: '180px',
-              mr: (id + 1) % 3 === 0 ? '0' : '16px',
-              mb: '16px',
-              verticalAlign: 'top',
-              cursor: 'pointer',
               borderRadius: '8px',
-              border: '1px solid',
+              border: '1px dashed',
               borderColor: 'gray.300',
-              _hover: {
-                borderColor: 'gray.500'
-              }
+              verticalAlign: 'top',
+              _hover: { borderColor: 'gray.500' }
             }}
           >
-            <MiniCanvas canvasObj={hex2Canvas(api.registry, value)} />
+            <Center
+              sx={{ h: '100%', color: 'gray.300', cursor: 'pointer', _hover: { color: 'gray.500' } }}
+              onClick={createNew}
+            >
+              <Icon as={FaPlusCircle} boxSize={10} />
+            </Center>
           </Box>
-        ))}
-        <Box
-          sx={{
-            display: 'inline-block',
-            w: '320px',
-            h: '180px',
-            borderRadius: '8px',
-            border: '1px dashed',
-            borderColor: 'gray.300',
-            verticalAlign: 'top',
-            _hover: { borderColor: 'gray.500' }
-          }}
-        >
-          <Center
-            sx={{ h: '100%', color: 'gray.300', cursor: 'pointer', _hover: { color: 'gray.500' } }}
-            onClick={createNew}
-          >
-            <Icon as={FaPlusCircle} boxSize={10} />
-          </Center>
-        </Box>
+        </SimpleGrid>
       </Box>
     </Center>
   );
