82488536d9b3d25bbca7c78d69f09a6dbdec0807
clearloop <udtrokia@gmail.com>
2021-02-06 16:07:57 +0800

    feat(page/lottery): add pagination for T

--- packages/page-lottery/src/table.tsx
+++ packages/page-lottery/src/table.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Table, Text, Thead, Th, Tr } from '@patract/ui-components';
+import { Box, Flex, Table, Text, Thead, Th, Tr } from '@patract/ui-components';
+import Pagination from '@material-ui/lab/Pagination';
 
 export interface Col {
   name: string;
@@ -9,9 +10,22 @@ export interface TableProps {
   cols: Col[];
   height?: string;
   title: string;
+
+  onChange?: (page: number) => void;
 }
 
-export const T: React.FC<TableProps> = ({ cols, height = '600px', title }) => {
+export const T: React.FC<TableProps> = ({ cols, height = '600px', title, onChange }) => {
+  const [page, setPage] = React.useState(1);
+
+  // On Changing Page
+  const c = React.useCallback(
+    (_: any, page: number) => {
+      setPage(page);
+      onChange && onChange(page);
+    },
+    [onChange, setPage]
+  );
+
   return (
     <Box height={height} mt='10' px='10px' py='8px' rounded='lg' shadow='sm' bg='white'>
       <Text p={8} fontSize='16px' fontWeight='bold'>
@@ -26,6 +40,9 @@ export const T: React.FC<TableProps> = ({ cols, height = '600px', title }) => {
           </Tr>
         </Thead>
       </Table>
+      <Flex mt='4' justifyContent='flex-end' position='relative' bottom='2'>
+        <Pagination count={9} page={page} onChange={c} shape='rounded' />
+      </Flex>
     </Box>
   );
 };
