6b90302eeaa8f4dd7592486ca4a6bf3c7cf6304c
iiiiiiiii <69485494+ii-ii-ii@users.noreply.github.com>
2021-02-20 21:30:09 +1300

    update

--- packages/page-uniswap/src/hooks/useFactoryContract.ts
+++ packages/page-uniswap/src/hooks/useFactoryContract.ts
@@ -2,5 +2,5 @@ import { useContract } from '@patract/react-hooks';
 import Factory from '../contracts/factory.json';
 
 export const useFactoryContract = () => {
-  return useContract('5C8pqeB3Cj1ePZm49D1wFwHAJtZARviCGhXkpGtSJSmdgHm7', Factory);
+  return useContract('5HBv8M3cmQvS5CWBaJSitwwvvKxYDmconuupEPLzFvMKqe89', Factory);
 };
