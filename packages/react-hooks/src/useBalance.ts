f9494d522a0e43643a841931ed4f7a8b84170d9a
iiiiiiiii <69485494+ii-ii-ii@users.noreply.github.com>
2021-02-21 21:42:08 +1300

    fix

--- packages/react-hooks/src/useBalance.ts
+++ packages/react-hooks/src/useBalance.ts
@@ -6,9 +6,13 @@ export const useBalance = (account: string) => {
   const [balance, setBalance] = useState<string | null>(null);
 
   useEffect(() => {
-    api.query.system.account(account, (info) => {
-      setBalance(info.data.free.toString());
-    });
+    api.query.system
+      .account(account, (info) => {
+        setBalance(info.data.free.toString());
+      })
+      .catch(() => {
+        setBalance(null);
+      });
   }, [api, account]);
 
   return balance;
