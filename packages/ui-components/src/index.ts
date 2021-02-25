46eb933842cf0ad1c82edefa7ae8b043c898e9fc
iiiiiiiii <69485494+ii-ii-ii@users.noreply.github.com>
2021-02-25 21:58:36 +1300

    update

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -7,6 +7,7 @@ export * from './identity-icon';
 export * from './address';
 export * from './amount';
 export * from './fixed';
+export * from './tag';
 
 export * from '@chakra-ui/icons';
 export * from '@chakra-ui/react';
