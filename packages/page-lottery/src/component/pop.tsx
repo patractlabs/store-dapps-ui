22364e21d7ae43c634dd06e5be3c95adca4d7db5
freepoix <17864213831@163.com>
2021-03-02 14:23:08 +0800

    lottery rules

--- packages/page-lottery/src/component/pop.tsx
+++ packages/page-lottery/src/component/pop.tsx
@@ -24,7 +24,7 @@ export const Pop: React.FC<{}> = () => {
           <Box mb='0.5rem'>1. Wrong with 3 numbers: lose all of your deposit.</Box>
           <Box mb='0.5rem'>2. Right with 1 number : get 2x of your deposit back.</Box>
           <Box mb='0.5rem'>3. Right with 2 numbers : get 10x of your deposit back.</Box>
-          <Box mb='0.5rem'>4. Right with 3 numbers : split half of the Reward Pool with other same level winners. </Box>
+          <Box mb='0.5rem'>4. Right with 3 numbers : split all the Reward Pool with other same level winners.</Box>
           <Box mb='0.5rem'>5. If the Reward Pool is empty, then all the left winers will get 0 back.</Box>
           <Box>6. After an epoch is ended, everyone can close this season and get 0.1 DOT back as reward.</Box>
         </PopoverBody>
