699d5c41aa00e1b8bebc2073fe4d82b276678c40
iiiiiiiii <69485494+ii-ii-ii@users.noreply.github.com>
2021-03-04 16:02:28 +1300

    update

--- packages/utils/src/contracts.ts
+++ packages/utils/src/contracts.ts
@@ -1,10 +1,22 @@
-export const PatraPK = '5ELRFnm3T9u1PSNaidPUbFj9cSATaYQX5zAeFTfFCFRSFQ4T';
-export const PatraPixel = '5H1b2NdvAJ2wiknBQ9rzryVHgDNBZiYQvRe35fuMSRXNMyEA';
-export const PatraLottery = '5FcjY9tStk6LjTsFCUDZXFPWugrkVaonwcdppVh6xi58QD4j';
-export const PatraSwap = '5FT3VC9mP7uZd1Nsj5Bm39adPgZeADvtVW84V7tYcci51yHM';
-export const PatraMaker = '5H71R2u6HuKCZVoxHJGK5TjJ5f51hzSvpZwQHo3qpHgyYf1m';
-export const USDT = '5HPfwG4sjRMEDRi1cbotXYfJM9Rxowi6RfX44x5K3CSm827r';
-export const JBTC = '5DXJYYYvN79J2nP3y5FXafUnn432pYgxvmLYEtfoBwTjXBRi';
-export const JETH = '5CxdigZ91UXiorv2L3VhGPRd9DafmkARdFHntpShtPQG5rGS';
-export const DAI = '5GbtNisL8n9ovjJEDN9prGHdvkczbWoy98K8EYA5U14p4ocD';
-export const EMPTY = '5C4hrfjw9DjXZTzV3MwzrrAr9P1MJhSrvWGWqi1eSuyUpnhM';
+// export const PatraPK = '5ELRFnm3T9u1PSNaidPUbFj9cSATaYQX5zAeFTfFCFRSFQ4T';
+// export const PatraPixel = '5H1b2NdvAJ2wiknBQ9rzryVHgDNBZiYQvRe35fuMSRXNMyEA';
+// export const PatraLottery = '5FcjY9tStk6LjTsFCUDZXFPWugrkVaonwcdppVh6xi58QD4j';
+// export const PatraSwap = '5FT3VC9mP7uZd1Nsj5Bm39adPgZeADvtVW84V7tYcci51yHM';
+// export const PatraMaker = '5H71R2u6HuKCZVoxHJGK5TjJ5f51hzSvpZwQHo3qpHgyYf1m';
+// export const USDT = '5HPfwG4sjRMEDRi1cbotXYfJM9Rxowi6RfX44x5K3CSm827r';
+// export const JBTC = '5DXJYYYvN79J2nP3y5FXafUnn432pYgxvmLYEtfoBwTjXBRi';
+// export const JETH = '5CxdigZ91UXiorv2L3VhGPRd9DafmkARdFHntpShtPQG5rGS';
+// export const DAI = '5GbtNisL8n9ovjJEDN9prGHdvkczbWoy98K8EYA5U14p4ocD';
+// export const EMPTY = '5C4hrfjw9DjXZTzV3MwzrrAr9P1MJhSrvWGWqi1eSuyUpnhM';
+
+
+export const PatraPK = "3djs7v6bJVuNtVuAE7K6YqUMGfskhcLC4wJYQXNRnZudkNUt";
+export const PatraPixel = "3gR2tVyU1e3KDpJkudncpZEVLT5UgnU5uNmwEjcY2k1Zrykh";
+export const PatraLottery = "3f2BQHDzk66iEXPphx9BUq8iZva3cejTvZmiyZQHZ2ZKu7Ac";
+export const PatraSwap = "3erVMKVKETuw84uTEZ7NzjKq3vGwHHrZUTFxeBbjCwCGWjs5";
+export const PatraMaker = "3gWTHAEe9FKa4ZLXnnBw33UVjtnJq4NboX5JSrm2QcBB3TBP";
+export const USDT = "3go7oPQRamMbiVEb85jWV8QW1P9Fw1dmQcexE1nVdWvxcqFq";
+export const JBTC = "3cvkQftUDT9fXqudUZB9YFDzSHkKwccduiUSPxNynFww2BAS";
+export const JETH = "3cN5aotgrpY6JvSbqXRKDyApoTHxtp66cCRh3t9tUhtTanHL";
+export const DAI = "3g1LErCsz8ABRnpoir5Sor2qazLHiaje85T2PbsG4KZ1ZdFV";
+export const EMPTY = "3bU9io5UzZju4XX4YqscpRv3ocieRmNXuTQQzmiq3ETgKhGV";
